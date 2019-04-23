76664eded881ca524aaafd42d35370bf84c443fb
Aidar Shaifutdinov <aidarshaifutdinov@gmail.com>
2019-04-23 21:58:03 +0300

    Initialize maven-based Java Spring web project

---
+++ src/main/java/org/doit/controller/MainController.java
@@ -0,0 +1,15 @@
+package org.doit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("msg", "Digital education map");
+        return "/index";
+    }
+}
