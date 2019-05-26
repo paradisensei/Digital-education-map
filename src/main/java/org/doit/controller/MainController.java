474035d5b5b465bb2aa6eed12c29f5a728caf6c0
guidefinder777 <amoore133@yandex.ru>
2019-05-26 23:30:27 +0300

    Map API Integration

--- src/main/java/org/doit/controller/MainController.java
+++ src/main/java/org/doit/controller/MainController.java
@@ -1,15 +1,21 @@
 package org.doit.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@PropertySource("classpath:application.properties")
 @Controller
 public class MainController {
+    @Value("${yandex.key}")
+    private String yandexKey;
 
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("msg", "Digital education map");
+        model.addAttribute("yandexKey", yandexKey);
         return "/index";
     }
 }
